756ab491f91cdf88c94a15dfe58c4dc663a8c390
chaudharypraveen98 <chaudharypraveen98@gmail.com>
2022-10-15 16:41:18 +0530

    adding console , window and web sys initialize

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
+use web_sys::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -9,11 +10,37 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
+extern  "C" {
     fn alert(s: &str);
 }
-
 #[wasm_bindgen]
 pub fn greet() {
+    using_web_sys();
     alert("Hello, {{project-name}}!");
+}
+// And finally, we don't even have to define the `log` function ourselves! The
+// `web_sys` crate already has it defined for us.
+
+fn using_web_sys() {
+    console::log_1(&"Hello using web-sys".into());
+
+    let js: JsValue = 4.into();
+    console::log_2(&"Logging arbitrary values looks like".into(), &js);
+}
+// Called when the wasm module is instantiated
+#[wasm_bindgen(start)]
+pub fn main() -> Result<(), JsValue> {
+    // Use `web_sys`'s global `window` function to get a handle on the global
+    // window object.
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+
+    // Manufacture the element we're gonna append
+    let val = document.create_element("p")?;
+    val.set_inner_html("Hello from Rust!");
+
+    body.append_child(&val)?;
+
+    Ok(())
 }
